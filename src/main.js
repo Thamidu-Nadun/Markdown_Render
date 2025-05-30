7f81fb5cc75c22e2531358921a061a4f67c23df8
Thamidu-Nadun <nadunrz101@gmail.com>
2025-05-30 23:58:51 +0530

    Add markdown-it-emoji plugin to support emojis in markdown rendering.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import MarkdownIt from "markdown-it";
 import markdownItFootNote from "markdown-it-footnote";
 import MarkdownItContainer from "markdown-it-container";
+import { full as emoji } from 'markdown-it-emoji'
 import { boxes } from "./lib/custom_calloutBox.js";
 
 const md = new MarkdownIt({
     breaks: true,
     linkify: true,
 })
-    .use(markdownItFootNote);
+    .use(markdownItFootNote)
+    .use(emoji);
 
 boxes.forEach(({ name, render }) => {
     md.use(MarkdownItContainer, name, { render });
